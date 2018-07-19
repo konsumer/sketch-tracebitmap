7a1300ff7d0251db46a571666b2bde08faaffa45
David Konsumer <konsumer@jetboystudio.com>
2018-07-19 14:27:15 -0700

    fleshed out export, but it beachballs

--- src/trace-image.js
+++ src/trace-image.js
@@ -1,11 +1,35 @@
+/* global NSBitmapImageRep, NSString, NSUTF8StringEncoding, MSSVGImporter */
+import { imagedataToSVG } from 'imagetracerjs'
+
 export default ({ document, selection }) => {
+  if (!selection) {
+    return document.showMessage('You must select at least one image')
+  }
+
   const images = Array.from(selection).filter(sel => sel.class() == 'MSBitmapLayer')
 
   if (!images.length) {
     return document.showMessage('You must select at least one image')
   }
 
-  images.forEach(i => {
-    console.log('image', i.fileName)
+  images.forEach(imageLayer => {
+    const imageRep = NSBitmapImageRep.imageRepWithData(imageLayer.image().data())
+    const width = imageRep.size().width
+    const height = imageRep.size().height
+    const data = imageLayer.image().data().toString()
+
+    const svgCode = imagedataToSVG({
+      width,
+      height,
+      data
+    }, {scale: 5})
+
+    const svgString = NSString.stringWithString(svgCode)
+    const svgData = svgString.dataUsingEncoding(NSUTF8StringEncoding)
+    const svgImporter = MSSVGImporter.svgImporter()
+    svgImporter.prepareToImportFromData(svgData)
+    const svgLayer = svgImporter.importAsLayer()
+    svgLayer.setName('SVG Layer')
+    document.currentPage().addLayers([svgLayer])
   })
 }
